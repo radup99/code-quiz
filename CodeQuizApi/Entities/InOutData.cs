e039da63f480543a9d41e6f2fecaea61fbb72872
radup99 <raduplacinta85@gmail.com>
2023-07-30 15:05:30 +0300

    add Problem, InOutData classes

---
+++ CodeQuizApi/Entities/InOutData.cs
@@ -0,0 +1,9 @@
+﻿namespace CodeQuizApi.Entities
+{
+    public class InOutData
+    {
+        public string Input { get; set; }
+
+        public string Output { get; set; }
+    }
+}
---
+++ CodeQuizApi/Entities/Problem.cs
@@ -0,0 +1,15 @@
+﻿namespace CodeQuizApi.Entities
+{
+    public class Problem
+    {
+        public int Id { get; set; }
+
+        public string AuthorId { get; set; }
+
+        public string Title { get; set; }
+
+        public string Text { get; set; }
+
+        public List<InOutData> InOutDataList { get; set; }
+    }
+}
