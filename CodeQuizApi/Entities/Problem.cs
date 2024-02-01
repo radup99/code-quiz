4bc217557c07fb495e58c8dd64252b8307953885
radup99 <raduplacinta85@gmail.com>
2024-02-01 16:10:42 +0200

    create Problem and InOutData classes

---
+++ CodeQuizApi/Entities/Problem.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Components.Forms;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CodeQuizApi.Entities
+{
+    public class Problem
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string AuthorId { get; set; }
+
+        public string Title { get; set; }
+
+        public string Text { get; set; }
+
+        public string InOutDataString { get; set; }
+
+        [NotMapped]
+        public List<InOutData> InOutDataList { get; set; }
+    }
+}
